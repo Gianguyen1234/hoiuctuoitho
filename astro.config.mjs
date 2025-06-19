5544d7be417f61185fef8a57a5fc88648fd1b14b
Thao Nguyen <nguyenluthao252@gmail.com>
2025-06-19 22:33:56 +0700

    Update Astro configuration for deployment and add gh-pages dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gianguyen1234.github.io/hoiuctuoitho/',
+  base: '/hoiuctuoitho',
+  output: 'static',
+  outDir: 'dist',
   integrations: [tailwind(), react()],
 });
